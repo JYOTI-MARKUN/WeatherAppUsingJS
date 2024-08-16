200aa6adff93050c2d9d76318d046bff7c8f547c
JYOTI-MARKUN <jyotimarkun1997@gmail.com>
2024-08-16 11:27:38 +0530

    tmpreture update

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ function weatherData(name){
             removeClass.classList.remove("weather-data")
             invalidCity.classList.add("invalid-city")
             cityName.innerText = data.name
-            tempretureValue.innerText = data.main.temp
+            tempretureValue.innerText = Math.floor(data.main.temp)
             tempFeelsLike.innerText = `Feels like ${data.main.feels_like}`
             console.log(data.main.humidity)
             humidValue.innerText = `${data.main.humidity}%`
